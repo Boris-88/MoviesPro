b7830aa62bf179a4579fe0cf77d80436715da921
Boris Zverik <Boris.zverik@gmail.com>
2024-05-20 13:31:54 +0300

    init

---
+++ MoviesPro/MoviesProApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesProApp.swift
+//  MoviesPro
+//
+//  Created by Boris Zverik on 20.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct MoviesProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
